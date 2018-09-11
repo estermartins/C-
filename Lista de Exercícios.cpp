fe7cf9c88f23359f3d390e757c42517e00005c96
Ester Pereira Martins <37341234+estermartins@users.noreply.github.com>
2018-09-11 14:14:22 -0300

    Create Lista de Exercícios.cpp

---
+++ Lista de Exercícios.cpp
@@ -0,0 +1,15 @@
+/******************************************************************************
+ Exercício 1 - Crie um programa em C++, que recebe uma palavra como parâmetro e imprime.
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string batata;
+    cout << "Digite uma palavra: " << "\n";
+    cin >> batata;
+    cout << "A parlavra digitada foi: " << batata << "\n";
+}
