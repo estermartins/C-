a5935d3c506a906235a6ebfcdfe1c4c32d366154
Ester Pereira Martins <37341234+estermartins@users.noreply.github.com>
2018-09-13 14:34:38 -0300

    Update Lista de Exercícios.cpp

--- Lista de Exercícios.cpp
+++ Lista de Exercícios.cpp
@@ -129,3 +129,76 @@ int main()
 Exercício 7 - Crie um vetor de elementos do tipo float, insira valores nesses elementos e faça um
 loop para imprimir cada um desses elementos.
 *******************************************************************************/
+
+/******************************************************************************
+Exercício 8 - Três times de futebol participaram de 6 partidas. Crie vetores para simular os resultados 
+desses jogos e imprima a pontuação obtida por cada time. 
+*******************************************************************************/
+
+/******************************************************************************
+Exercício 9 - Cria um programa em C++ que possui uma variável inteira que se inicia com o valor 0. 
+Faça com que ela acumule o valor 2 nela mesma ao longo de 20 iterações. 
+*******************************************************************************/
+
+/******************************************************************************
+Exercício 10 - Pensando na mesma lógica do programa anterior, crie uma alteração no Exercício referente 
+aos times, para que seja feita a soma das pontuações de cada um. 
+*******************************************************************************/
+
+/******************************************************************************
+Exercício 11 - Crie um programa que faz a leitura da nota final de um aluno e verifica se ele  
+foi aprovado, reprovado ou se está de exame especial em uma disciplina. Os critérios que devem 
+ser considerados são: Nota acima de 70 indica aprovação, nota abaixo de 40 indica reprovação e 
+nota entre 40 e 59 indica exame especial.  
+*******************************************************************************/
+
+/******************************************************************************
+Exercício 12 - Faça uma alteração no seu programa anterior, para que ele utilize as notas individuais 
+do aluno em cada atividade da disciplina, e calcule o resultado final, para que posteriormente possa 
+ser verificada a aprovação do aluno.   
+*******************************************************************************/
+
+/******************************************************************************
+Exercício 13 - Um desenvolvedor recebe 50 reais por hora trabalhada. Considerando que ele trabalhada 
+8 horas por dia, crie uma função que calcula quanto esse desenvolvedor recebeu no mês atual, considerando 
+que ele trabalhou 22 dias nesse mês. 
+*******************************************************************************/
+
+/******************************************************************************
+Exercício 14 - Uma empresa de aviação possui um tipo de avião que comporta 10 pessoas. Crie um programa 
+que possui uma função, cujos parâmetros de entrada são: o número de aviões disponíveis e o número de 
+passageiros para serem transportados. Essa função deverá calcular se os aviões conseguem comportar essas 
+pessoas, e caso não consiga, quantas ficarão para as próximas viagens. Faça uma simulação na qual os 
+aviões comportam o número de pessoas e outra na qual ele não comporta. 
+*******************************************************************************/
+
+/******************************************************************************
+Exercício 15 - Um hospital consegue atender 20 pessoas por hora. Considerando que no momento em que 
+o hospital foi aberto chegaram 100 pessoas, faça um programa para calcular quanto tempo o paciente com 
+a senha 68 irá demorar para ser atendido.  
+*******************************************************************************/]
+
+/******************************************************************************
+Exercício 16 - Faça um programa em C++ que percorre um conjunto de números e informa qual deles é o 
+maior.   
+*******************************************************************************/
+
+/******************************************************************************
+Exercício 17 - Faça um programa em C++ que executa a mesma operação do primeiro, porém informa a 
+posição do maior número, e não o seu valor.   
+*******************************************************************************/
+
+/******************************************************************************
+Exercício 18 - Faça um ajuste no programa 16, para que ele também informe o menor valor da lista.  
+*******************************************************************************/
+
+/******************************************************************************
+Exercício 19 - A equipe de uma contrutora consegue entregar 100 metros quadrados construídos por dia. 
+Com base nisso, faça um programa que possui uma função para efetuar o cálculo do tempo necessário 
+para construir um projeto com 2326 metros quadrados. Esse tempo deve ser fornecido em dias.  
+*******************************************************************************/
+
+/******************************************************************************
+Exercício 20 - Gere um conjunto de 10 números inteiros de forma aleatória, e verifique qual é o maior 
+valor gerado. 
+*******************************************************************************/
