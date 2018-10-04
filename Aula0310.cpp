efa55a464f321f75b27aa8af1629e3d6f1560972
Ester Pereira Martins <37341234+estermartins@users.noreply.github.com>
2018-10-03 22:23:32 -0300

    Update Aula0310.cpp

--- Aula0310.cpp
+++ Aula0310.cpp
@@ -15,3 +15,21 @@ int main(){
     int vetor[6]={1,2,3,4,5,6};
     cout << max(vetor,6);
 }
+
+#include <iostream>
+using namespace std;
+
+int max(int vetor[], int tam){
+    int maior = vetor[0];
+    for(int contador = 1; contador < tam; contador++)
+    {
+        if(vetor[contador] > maior){
+            maior = vetor[contador];
+        }
+    }
+    return maior;
+}
+int main(){
+    int vetor[6]={1,2,3,4,5,6};
+    cout << max(vetor,6);
+}
