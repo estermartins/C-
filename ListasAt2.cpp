9dfb7e2437b5cd3c6abfcfd45c5890bced9aac72
Ester Pereira Martins <37341234+estermartins@users.noreply.github.com>
2018-11-30 19:37:54 -0200

    Create ListasAt2.cpp

---
+++ ListasAt2.cpp
@@ -0,0 +1,43 @@
+using namespace std;
+#include <iostream>
+
+// Imprime o vetor
+void imprime(char vetor[], char n) 
+{ 
+    char i; 
+    for (i=0; i < n; i++) 
+    	cout << vetor[i] << " "; 
+    cout << "\n"; 
+} 
+
+void ordenacaoPorInsercao(char vetor[], char n) 
+{ 
+    char i, chave, j; 
+    for (i = 1; i < n; i++) 
+    { 
+    	chave = vetor[i]; 
+    	j = i-1; 
+    
+    	/* Move os elementos do vetor[0 ... i-1] que são
+    	maiores do que a chave para uma posição a frente*/
+    	while (j >= 0 && vetor[j] > chave) 
+    	{ 
+    		vetor[j+1] = vetor[j]; 
+    		j = j-1; 
+    	} 
+    	vetor[j+1] = chave; 
+    	imprime(vetor, n);
+    } 
+} 
+
+int main() 
+{ 
+	char vetor[] = {'E', 'S', 'T', 'E', 'R', 'M', 'A','R','T', 'I', 'N', 'S'}; 
+	int n = sizeof(vetor)/sizeof(vetor[0]); 
+    cout << "Vetor Inicial:" << endl;
+    imprime(vetor, n);
+	ordenacaoPorInsercao(vetor, n); 
+	imprime(vetor, n); 
+
+	return 0; 
+}
